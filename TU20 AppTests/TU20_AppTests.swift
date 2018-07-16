7edb49d05e0242c23e81a69166c607cfd6c93e7c
Alexsia Louizos <alexsia.c.louizos@gmail.com>
2018-07-15 23:54:44 -0400

    Initial Commit

---
+++ TU20 AppTests/TU20_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  TU20_AppTests.swift
+//  TU20 AppTests
+//
+//  Created by Alexsia Louizos on 2018-07-15.
+//  Copyright © 2018 Dean Louizos. All rights reserved.
+//
+
+import XCTest
+@testable import TU20_App
+
+class TU20_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
